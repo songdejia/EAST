6f6e7382a7e6a576b0c97ae75240a5d8a8285959
dejiasong <dejiasong@gmail.com>
2018-09-17 00:12:43 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import config as cfg
 from utils.init import *
 from utils.util import *
 from utils.save import *
+from utils.myzip import *
 import torch.backends.cudnn as cudnn
 from eval import predict
 import zipfile
@@ -25,8 +26,6 @@ import numpy as np
 
 
 def train(train_loader, model, criterion, scheduler, optimizer, epoch):
-    print('*'*50)
-    print('Epoch {} / {}'.format(epoch + 1, cfg.max_epochs))
     start = time.time()
     losses = AverageMeter()
     batch_time = AverageMeter()
@@ -54,51 +53,55 @@ def train(train_loader, model, criterion, scheduler, optimizer, epoch):
         end = time.time()
 
         if i % cfg.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\n'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\n'.format(epoch, i, len(train_loader), loss=losses))
+            print('EAST <==> TRAIN <==> Epoch: [{0}][{1}/{2}] Loss {loss.val:.4f} Avg Loss {loss.avg:.4f})\n'.format(epoch, i, len(train_loader), loss=losses))
 
         save_loss_info(losses, epoch, i, train_loader)
 
 
 def main():
+    hmean = .0
+    is_best = False
+
     warnings.simplefilter('ignore', np.RankWarning)
     # Prepare for dataset
-    train_path = os.path.join(cfg.dataroot, 'train')
-    train_img = os.path.join(train_path, 'img')
-    train_gt  = os.path.join(train_path, 'gt')
+    print('EAST <==> Prepare <==> DataLoader <==> Begin')
+    train_root_path = os.path.abspath(os.path.join('./dataset/', 'train'))
+    train_img = os.path.join(train_root_path, 'img')
+    train_gt  = os.path.join(train_root_path, 'gt')
 
     trainset = custom_dset(train_img, train_gt)
-    train_loader = DataLoader(trainset, batch_size=cfg.train_batch_size, shuffle=True, collate_fn=collate_fn, num_workers=cfg.num_workers)
-    print('load img from {}'.format(train_path))
-    print('load gt from {}'.format(train_gt))
-    print('Data loader Done')
+    train_loader = DataLoader(trainset, batch_size=cfg.train_batch_size_per_gpu*cfg.gpu,
+        shuffle=True, collate_fn=collate_fn, num_workers=cfg.num_workers)
+    print('EAST <==> Prepare <==> Batch_size:{} <==> Begin'.format(cfg.train_batch_size_per_gpu*cfg.gpu))
+    print('EAST <==> Prepare <==> DataLoader <==> Done') 
+    
+
+    # test datalodaer
 
     # Model
+    print('EAST <==> Prepare <==> Network <==> Begin')
     model = East()
     model = nn.DataParallel(model, device_ids=cfg.gpu_ids)
     model = model.cuda()
     init_weights(model, init_type=cfg.init_type)
     cudnn.benchmark = True
-    print('Model initialization Done')
-
+    
+    criterion = LossFunc()
+    optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=10000, gamma=0.94)
+    
     # init or resume
     if cfg.resume and  os.path.isfile(cfg.checkpoint):
         weightpath = os.path.abspath(cfg.checkpoint)
-        print(weightpath)
-        print("=> loading checkpoint '{}'".format(weightpath))
+        print("EAST <==> Prepare <==> Loading checkpoint '{}' <==> Begin".format(weightpath))
         checkpoint = torch.load(weightpath)
         start_epoch = checkpoint['epoch']
         model.load_state_dict(checkpoint['state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer'])
-        print("=> loaded checkpoint '{}' (epoch {})".format(weightpath, checkpoint['epoch']))
+        print("EAST <==> Prepare <==> Loading checkpoint '{}' <==> Done".format(weightpath))
     else:
-        print("=> no checkpoint found at '{}'".format(cfg.checkpoint))
         start_epoch = 0
-
-    criterion = LossFunc()
-    optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
-    scheduler = lr_scheduler.StepLR(optimizer, step_size=10000, gamma=0.94)
-    print('Criterion and Optimizer is Done')
+    print('EAST <==> Prepare <==> Network <==> Done')
 
     for epoch in range(start_epoch, cfg.max_epochs):
 
@@ -106,23 +109,25 @@ def main():
 
         if epoch % cfg.eval_iteration == 0:
 
+            # create res_file and img_with_box
             output_txt_dir_path = predict(model, criterion, epoch)
-            print('epoch {} prediction'.format(epoch))
 
-            # zip and move to  compute F1
-            shutil.make_archive('epoch_{}_submit'.format(epoch),'zip',output_txt_dir_path+'/')
+            # Zip file
+            submit_path = MyZip(output_txt_dir_path, epoch)
 
-            command = 'mv epoch_{}_submit.zip ./submit'.format(epoch)
-            os.system(command)
+            # submit and compute Hmean
+            hmean_ = compute_hmean(submit_path)
 
-        if epoch % cfg.save_iteration == 0:
+            if hmean_ > hmean:
+                is_best = True
+
             state = {
-                    'epoch' : epoch + 1,
+                    'epoch'      : epoch,
                     'state_dict' : model.state_dict(),
                     'optimizer'  : optimizer.state_dict(),
+                    'is_best'    : is_best,
                     }
             save_checkpoint(state, epoch)
-            print('epoch {} saved'.format(epoch))
 
 
 
